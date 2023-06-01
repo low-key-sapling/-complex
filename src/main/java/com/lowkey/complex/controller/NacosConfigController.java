2442b6e6f8fa89be524c2052096c26be9c5bbacd
yuanjifan <yuanjifan@foxmail.com>
2023-06-01 17:16:10 +0800

    feature(added:集成Nacos配置中心):

---
+++ src/main/java/com/lowkey/complex/controller/NacosConfigController.java
@@ -0,0 +1,30 @@
+package com.lowkey.complex.controller;
+
+import com.lowkey.complex.response.ResultEntity;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/nacos")
+@RefreshScope
+public class NacosConfigController {
+
+    @Value("${user.name}")
+    private String name;
+    @Value("${user.age}")
+    private String age;
+
+    @GetMapping(value = "/config")
+    public ResultEntity<Map<String, String>> function() {
+        Map<String, String> entries = new HashMap<>();
+        entries.put("name", name);
+        entries.put("age", age);
+        return ResultEntity.successWithData(entries);
+    }
+}
